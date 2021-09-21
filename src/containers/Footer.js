ba98557d67e8a7a1972865f62f4dcc9ea33d5689
Yuliia Matvi <jualiasha@gmail.com>
2021-09-21 16:07:50 +0300

    footer added

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { NavHashLink } from "react-router-hash-link";
 
 const Footer = () => {
-  return <footer>here is footer</footer>;
+  return (
+    <footer>
+      <div className="footer">
+        <div className="footer__logo">RockShop</div>
+        <div className="footer__menu">
+          <ul>
+            Categoties
+            <li>
+              <NavHashLink to="/#igneousrocks">IgneousRocks</NavHashLink>
+            </li>
+            <li>
+              <NavHashLink to="/#metamorphicrocks">
+                MetamorphicRocks
+              </NavHashLink>
+            </li>
+            <li>
+              <NavHashLink to="/#minerals">Minerals</NavHashLink>
+            </li>
+            <li>
+              <NavHashLink to="/#sedimentaryrocks">
+                SedimentaryRocks
+              </NavHashLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <p className="footer__copywrite">Copywrite 2021</p>
+    </footer>
+  );
 };
 
 export default Footer;
